040e9e7674f43a1470d7d48cc41dc7a9f306283b
Harrison York <h.york11@gmail.com>
2022-01-14 08:30:30 -0600

    Finishing the walking skeleton

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,8 +32,9 @@ namespace API
             services.AddDbContext<DataContext>(options => {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
-
+            
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -53,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
